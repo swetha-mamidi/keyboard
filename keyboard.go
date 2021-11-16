b92c1d276a3402ec802846b91b542a30ec705ede
swetha-mamidi <swetha.m8@gmail.com>
2021-11-16 15:33:45 -0500

    Update keyboard.go

--- keyboard.go
+++ keyboard.go
@@ -9,15 +9,16 @@ import (
 )
 
 //UserInput function catures user input from keyboard prompting them with the given msg
+// Returns user input as string, error
 func UserInput(msg string) (string, error) {
-	//types.String
+	
 	fmt.Print(msg, " :")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
-	// clean up input and see if it is valid grade
+	// clean up input
 	input = strings.TrimSpace(input)
 	return input, nil
 }
